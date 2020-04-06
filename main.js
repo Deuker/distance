6b727b6f8e94ae896322c00b8cf54980fbcd8cea
Lucas Deuker <lucas.deuker@gmail.com>
2020-04-06 12:14:49 +0200

    First Test Upload

---
+++ main.js
@@ -0,0 +1,18 @@
+const game = New Game();
+const player = New Player();
+
+
+
+function preload() {
+
+}
+
+function setup() {
+    createCanvas(600, 600) {
+
+    }
+}
+
+function draw() {
+    game.player();
+}
---
+++ player.js
@@ -0,0 +1,7 @@
+class Player() {
+    constructor(x,y,image) {
+        this.x = x;
+        this.y = y;
+        this.image = loadImage("images/run Kopie.gif");
+    }
+}
